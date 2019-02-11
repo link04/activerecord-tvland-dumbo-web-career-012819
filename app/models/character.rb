ae4e88b3cb0585ecbc798c29a9842cd3ad76c552
Maximo Bautista <max.bautista97@gmail.com>
2019-02-11 01:01:29 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,7 +6,7 @@ class Character < ActiveRecord::Base
   "#{self.name} always says: #{self.catchphrase}"
   end
   
-  def build_show(show)
+  def .build_network(show)
     Show.create(show)
   end
   
