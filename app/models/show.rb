ca13cb2263d10f6dedbcd77c905b39871ca79fdd
Maximo Bautista <max.bautista97@gmail.com>
2019-02-11 01:09:01 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,8 +3,6 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   belongs_to :network
   
-  def build_network(network)
-    Network.create(network).shows << self
-  end
+
   
 end
