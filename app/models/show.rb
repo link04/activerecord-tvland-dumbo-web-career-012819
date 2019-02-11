78310165127cfb8e946615e97128198f5fb5fa31
Maximo Bautista <max.bautista97@gmail.com>
2019-02-11 01:03:13 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,9 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   belongs_to :network
+  
+  def build_network(network)
+    Network.create(show)
+  end
+  
 end
